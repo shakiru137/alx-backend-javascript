e3e240bd8155bc83d427f312ffa5fc90cc34bcc6
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2024-08-07 09:50:04 +0000

    shorthand for objects with same key and value

---
+++ 0x00-ES6-basic/7-getBudgetObject.js
@@ -0,0 +1,9 @@
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income,  // shorthand for income: income
+    gdp,  // shorthand for gdp: gdp
+    capita,  // shorthand for capita: capita
+  };
+
+  return budget;
+}
